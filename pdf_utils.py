44db44dedbbc84da8e85160f65595aa2b3359f7d
Teinkkkkkkkkkkk <33937631+TeinkBR@users.noreply.github.com>
2023-04-08 22:54:13 +0200

    Update pdf_utils.py

--- pdf_utils.py
+++ pdf_utils.py
@@ -1,6 +1,10 @@
 from pdf2image import convert_from_path
 from PIL import Image
 import pytesseract
+from pdfminer.layout import LAParams
+from pdfminer.converter import PDFPageAggregator
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
 
 
 def pdf_to_images(pdf_path):
@@ -22,3 +26,22 @@ def image_to_text(image):
     """Extracts text from a PIL Image object using OCR."""
     text = pytesseract.image_to_string(image)
     return text
+
+
+def extract_layout(pdf_path):
+    """Extracts layout information (position, font size, etc.) from a PDF file using pdfminer."""
+    resource_manager = PDFResourceManager()
+    laparams = LAParams()
+    device = PDFPageAggregator(resource_manager, laparams=laparams)
+    interpreter = PDFPageInterpreter(resource_manager, device)
+
+    layout_data = []
+
+    with open(pdf_path, "rb") as f:
+        for page in PDFPage.get_pages(f):
+            interpreter.process_page(page)
+            layout = device.get_result()
+            # Extract layout information (position, font size, etc.) and store it in layout_data
+            layout_data.append(layout)
+
+    return layout_data
