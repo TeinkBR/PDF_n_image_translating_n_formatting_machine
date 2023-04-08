c5816dc4ecfbaaf8b700cf31ed7d8201c90fc3ac
Teinkkkkkkkkkkk <33937631+TeinkBR@users.noreply.github.com>
2023-04-08 22:50:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import pdf_utils
 # Convert a PDF file to a list of PIL Image objects
 pdf_images = pdf_utils.pdf_to_images('path/to/your/pdf')
 
-# Preprocess each image in the list
-preprocessed_images = []
+# Preprocess and OCR each image in the list
+extracted_texts = []
 for image in pdf_images:
     preprocessed_image = pdf_utils.preprocess_image(image)
-    preprocessed_images.append(preprocessed_image)
+    text = pdf_utils.image_to_text(preprocessed_image)
+    extracted_texts.append(text)
 
-# Now you can feed the preprocessed images to your OCR and translation pipeline
+# Now you can feed the extracted texts to your translation pipeline
