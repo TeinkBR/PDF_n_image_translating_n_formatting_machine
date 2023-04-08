436b6026bac6aab6456a1ebfd8c6fc9d8bbdc7a4
Teinkkkkkkkkkkk <33937631+TeinkBR@users.noreply.github.com>
2023-04-08 23:00:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,30 @@
 import pdf_utils
 import opennmt
-import PyPDF2
-from reportlab.pdfgen import canvas
 
 
-# Load the trained translation model
-model = opennmt.load_model("path/to/your/trained/model")
+def main():
+    # Paths to input and output PDF files
+    pdf_path = "path/to/your/pdf"
+    output_path = "path/to/your/output_pdf"
+
+    # Load the trained translation model
+    model = opennmt.load_model("path/to/your/trained/model")
+
+    # Convert the PDF file to a list of preprocessed images and extract layout information
+    images = pdf_utils.pdf_to_images(pdf_path)
+    layout_data = pdf_utils.extract_layout(pdf_path)
+
+    # Extract text from each image and translate it using the pre-trained model
+    translated_texts = []
+    for image in images:
+        preprocessed_image = pdf_utils.preprocess_image(image)
+        text = pdf_utils.image_to_text(preprocessed_image)
+        translated_text = pdf_utils.translate_text(text, model)
+        translated_texts.append(translated_text)
+
+    # Generate a new PDF with the translated text in the same format as the original PDF
+    pdf_utils.create_translated_pdf(layout_data, translated_texts, output_path)
 
 
-# Convert a PDF file to a list of preprocessed images and extract text from each image
-pdf_images = pdf_utils.pdf_to_images('path/to/your/pdf')
-extracted_texts = []
-for image in pdf_images:
-    preprocessed_image = pdf_utils.preprocess_image(image)
-    text = pdf_utils.image_to_text(preprocessed_image)
-    extracted_texts.append(text)
-
-
-# Extract layout information (position, font size, etc.) from the original PDF
-layout_data = pdf_utils.extract_layout('path/to/your/pdf')
-
-
-# Translate each extracted text using the pre-trained model
-translated_texts = []
-for text in extracted_texts:
-    translated_text = model.translate(text)
-    translated_texts.append(translated_text)
-
-
-# Finally, you can generate a new PDF with the translated text in the same format as the original PDF
-pdf_utils.create_translated_pdf(layout_data, translated_texts, 'path/to/your/output_pdf')
+if __name__ == "__main__":
+    main()
