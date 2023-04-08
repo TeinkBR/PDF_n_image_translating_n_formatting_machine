48ce5ee3b3fde23f63f2e622302a9d34b311b083
Teinkkkkkkkkkkk <33937631+TeinkBR@users.noreply.github.com>
2023-04-09 00:01:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,19 +8,13 @@ def main():
     pdf_path = "path/to/your/pdf"
     output_path = "path/to/your/output_pdf"
 
-    # Load the trained translation model
+    # Load the trained translation model, CRAFT model, and CRNN model
     try:
         model = opennmt.load_model("path/to/your/trained/model")
-    except FileNotFoundError:
-        print("Error: Trained model file not found.")
-        return
-
-    # Load the pre-trained CRAFT and CRNN models
-    try:
         craft_model = deep_ocr.load_craft_model("path/to/your/craft/model")
         crnn_model = deep_ocr.load_crnn_model("path/to/your/crnn/model")
     except FileNotFoundError:
-        print("Error: CRAFT or CRNN model file not found.")
+        print("Error: Trained model, CRAFT, or CRNN model file not found.")
         return
 
     # Convert the PDF file to a list of preprocessed images and extract layout information
@@ -28,7 +22,7 @@ def main():
         images = pdf_utils.pdf_to_images(pdf_path)
         layout_data = pdf_utils.extract_layout(pdf_path)
     except FileNotFoundError:
-        print("Error: Input PDF file not found.")
+        print(f"Error: Input PDF file {pdf_path} not found.")
         return
 
     # Extract text from each image and translate it using the pre-trained model
