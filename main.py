c79ed06bd32ef9643a914e21fcaae1c31e0c2964
Teinkkkkkkkkkkk <33937631+TeinkBR@users.noreply.github.com>
2023-04-08 22:52:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,51 @@
 import pdf_utils
+import opennmt
 
 
-# Convert a PDF file to a list of PIL Image objects
+# Load the trained translation model
+model = opennmt.load_model("path/to/your/trained/model")
+
+
+# Convert a PDF file to a list of preprocessed images and extract text from each image
 pdf_images = pdf_utils.pdf_to_images('path/to/your/pdf')
-
-# Preprocess and OCR each image in the list
 extracted_texts = []
 for image in pdf_images:
     preprocessed_image = pdf_utils.preprocess_image(image)
     text = pdf_utils.image_to_text(preprocessed_image)
     extracted_texts.append(text)
 
-# Now you can feed the extracted texts to your translation pipeline
+
+# Translate each extracted text using the pre-trained model
+translated_texts = []
+for text in extracted_texts:
+    translated_text = model.translate(text)
+    translated_texts.append(translated_text)
+
+
+# Finally, you can place the translated text back into the PDF file
+# For this, you can use a library like PyPDF2 or pdfrw
+# You will need to adjust the code based on your specific PDF format and layout
+# Here's a simple example using PyPDF2:
+import PyPDF2
+
+# Open the original PDF file
+pdf_file = open('path/to/your/pdf', 'rb')
+pdf_reader = PyPDF2.PdfReader(pdf_file)
+pdf_writer = PyPDF2.PdfWriter()
+
+# Go through each page of the PDF file and replace the original text with the translated text
+for i, page in enumerate(pdf_reader.pages):
+    page_text = page.extract_text()
+    translated_text = translated_texts[i]
+    # Replace the original text with the translated text
+    page_text = page_text.replace(original_text, translated_text)
+    # Write the updated page to the PDF writer
+    pdf_writer.addPage(page_text)
+
+# Save the updated PDF file
+pdf_output = open('path/to/your/output_pdf', 'wb')
+pdf_writer.write(pdf_output)
+
+# Close the files
+pdf_file.close()
+pdf_output.close()
