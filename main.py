dfe748835e1792fed6b3cc2a992353db2c0d30f2
Teinkkkkkkkkkkk <33937631+TeinkBR@users.noreply.github.com>
2023-04-08 22:55:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pdf_utils
 import opennmt
+import PyPDF2
 
 
 # Load the trained translation model
@@ -15,6 +16,10 @@ for image in pdf_images:
     extracted_texts.append(text)
 
 
+# Extract layout information (position, font size, etc.) from the original PDF
+layout_data = pdf_utils.extract_layout('path/to/your/pdf')
+
+
 # Translate each extracted text using the pre-trained model
 translated_texts = []
 for text in extracted_texts:
@@ -25,22 +30,24 @@ for text in extracted_texts:
 # Finally, you can place the translated text back into the PDF file
 # For this, you can use a library like PyPDF2 or pdfrw
 # You will need to adjust the code based on your specific PDF format and layout
-# Here's a simple example using PyPDF2:
-import PyPDF2
 
 # Open the original PDF file
 pdf_file = open('path/to/your/pdf', 'rb')
-pdf_reader = PyPDF2.PdfReader(pdf_file)
-pdf_writer = PyPDF2.PdfWriter()
+pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+pdf_writer = PyPDF2.PdfFileWriter()
 
 # Go through each page of the PDF file and replace the original text with the translated text
 for i, page in enumerate(pdf_reader.pages):
-    page_text = page.extract_text()
+    page_text = page.extractText()
     translated_text = translated_texts[i]
     # Replace the original text with the translated text
-    page_text = page_text.replace(original_text, translated_text)
-    # Write the updated page to the PDF writer
-    pdf_writer.addPage(page_text)
+    page_text = page_text.replace(extracted_texts[i], translated_text)
+    # Create a new page with the updated text
+    new_page = PyPDF2.pdf.PageObject.createFromString(page_text)
+    # Copy the original page's layout (position, font size, etc.) to the new page
+    new_page.mergePage(page)
+    # Add the new page to the PDF writer
+    pdf_writer.addPage(new_page)
 
 # Save the updated PDF file
 pdf_output = open('path/to/your/output_pdf', 'wb')
