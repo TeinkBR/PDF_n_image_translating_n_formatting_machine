0041ad8c3f9d200558f2ebf67c53f2ed1dab90a1
Teinkkkkkkkkkkk <33937631+TeinkBR@users.noreply.github.com>
2023-04-09 01:12:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ import deep_ocr
 import opennmt
 from multiprocessing import Pool
 
+
 def process_image(image):
     preprocessed_image = pdf_utils.preprocess_image(image)
     text = pdf_utils.image_to_text(preprocessed_image, craft_model, crnn_model)
     return text
+
 
 def main():
     # Setup logging
@@ -18,29 +20,44 @@ def main():
 
     # Parse command-line arguments or load from config file
     parser = argparse.ArgumentParser(description="Translate PDF documents using OCR and deep learning.")
-    parser.add_argument("--input", type=str, required=True, help="Input PDF file path.")
-    parser.add_argument("--output", type=str, required=True, help="Output translated PDF file path.")
-    parser.add_argument("--model", type=str, required=True, help="Path to the deep learning model.")
-    parser.add_argument("--craft-model", type=str, required=True, help="Path to the CRAFT model.")
-    parser.add_argument("--crnn-model", type=str, required=True, help="Path to the CRNN model.")
-    parser.add_argument("--dpi", type=int, default=300, help="DPI of the output images.")
+    parser.add_argument("--config", type=str, default="config.json", help="Path to the configuration file.")
+    parser.add_argument("--input", type=str, help="Input PDF file path.")
+    parser.add_argument("--output", type=str, help="Output translated PDF file path.")
+    parser.add_argument("--model", type=str, help="Path to the deep learning model.")
+    parser.add_argument("--craft-model", type=str, help="Path to the CRAFT model.")
+    parser.add_argument("--crnn-model", type=str, help="Path to the CRNN model.")
+    parser.add_argument("--dpi", type=int, help="DPI of the output images.")
+    parser.add_argument("--target-language", type=str, help="Target language code for translation.")
     args = parser.parse_args()
+
+    # Load settings from the configuration file
+    with open(args.config, "r") as f:
+        config = json.load(f)
+
+    # Override configuration settings with command-line arguments, if provided
+    input_pdf_path = args.input or config["input_pdf_path"]
+    output_pdf_path = args.output or config["output_pdf_path"]
+    model_path = args.model or config["model_path"]
+    craft_model_path = args.craft_model or config["craft_model_path"]
+    crnn_model_path = args.crnn_model or config["crnn_model_path"]
+    dpi = args.dpi or config["dpi"]
+    target_language = args.target_language or config["target_language"]
 
     # Load the trained translation model, CRAFT model, and CRNN model
     try:
-        model = opennmt.load_model(args.model)
-        craft_model = deep_ocr.load_craft_model(args.craft_model)
-        crnn_model = deep_ocr.load_crnn_model(args.crnn_model)
+        model = opennmt.load_model(model_path, target_language)
+        craft_model = deep_ocr.load_craft_model(craft_model_path)
+        crnn_model = deep_ocr.load_crnn_model(crnn_model_path)
     except FileNotFoundError:
         logging.error("Trained model, CRAFT, or CRNN model file not found.")
         return
 
     # Convert the PDF file to a list of preprocessed images and extract layout information
     try:
-        images = pdf_utils.pdf_to_images(args.input, args.dpi)
-        layout_data = pdf_utils.extract_layout(args.input)
+        images = pdf_utils.pdf_to_images(input_pdf_path, dpi)
+        layout_data = pdf_utils.extract_layout(input_pdf_path)
     except FileNotFoundError:
-        logging.error(f"Input PDF file {args.input} not found.")
+        logging.error(f"Input PDF file {input_pdf_path} not found.")
         return
 
     # Extract text from each image using OCR in parallel
@@ -49,17 +66,18 @@ def main():
 
     # Translate the extracted texts using the pre-trained model
     try:
-        translated_texts = pdf_utils.translate_texts(texts, model)
+        translated_texts = pdf_utils.translate_texts(texts, model, target_language)
     except Exception as e:
         logging.error(f"Error: {e}")
         return
 
     # Generate a new PDF with the translated text in the same format as the original PDF
     try:
-        pdf_utils.create_translated_pdf(layout_data, translated_texts, args.output)
+        pdf_utils.create_translated_pdf(layout_data, translated_texts, output_pdf_path)
     except Exception as e:
         logging.error(f"Error: {e}")
         return
+
 
 if __name__ == "__main__":
     main()
